9381d5d7ca9ecdc37458b28c73be202e72edc536
Itssuraj <thapasuraj3215@gmail.com>
2026-09-14 20:48:20 +0545

    scafold

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter ',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
--- lib/view/homeview.dart
+++ lib/view/homeview.dart
@@ -17,7 +17,33 @@ class _HomeViewState extends State<HomeView> {
       ),
       body: Column(
         children: [
-          Text("Hello, World!"),
+          //w1
+          Container(
+            width: double.infinity,
+            height: 70,
+            color: Colors.blue,
+            child: Image.network(
+              "https://images.unsplash.com/photo-1773332611476-6ec2ba68049f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+            ),
+          ),
+          SizedBox(height: 20),
+
+          //w2
+           Text("Get job-ready with live classes,\n lifetime access, and a guaranteed internship - all for less than Rs.2,500."),
+
+          //w3 + w4
+          FilledButton(
+            onPressed: () {
+              print("Button Pressed");
+            },
+            child: const Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.home),
+                Text("Home"),
+              ],
+            ),
+          ),
         ],
       ),
     );
