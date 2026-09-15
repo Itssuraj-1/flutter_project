e8ecd5007dfa1ff4b9eb99ede9ae29c9536160a2
Itssuraj <thapasuraj3215@gmail.com>
2026-09-15 19:27:45 +0545

    Refactor HomeView layout and improve UI components

--- lib/view/homeview.dart
+++ lib/view/homeview.dart
@@ -1,4 +1,3 @@
-// lib/view/homeview.dart
 import 'package:flutter/material.dart';
 
 class HomeView extends StatefulWidget {
@@ -11,40 +10,112 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Itssuraj"),
-      ),
-      body: Column(
-        children: [
-          //w1
-          Container(
-            width: double.infinity,
-            height: 70,
-            color: Colors.blue,
-            child: Image.network(
-              "https://images.unsplash.com/photo-1773332611476-6ec2ba68049f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
-            ),
+    return SafeArea(
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("About"),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Setting"),
+                trailing: Icon(Icons.settings),
+              ),
+              Divider(),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout"),
+                onTap: () {},
+              ),
+            ],
           ),
-          SizedBox(height: 20),
+        ),
 
-          //w2
-           Text("Get job-ready with live classes,\n lifetime access, and a guaranteed internship - all for less than Rs.2,500."),
+        appBar: AppBar(
+          title: Text("Code IT"),
+        ),
 
-          //w3 + w4
-          FilledButton(
-            onPressed: () {
-              print("Button Pressed");
-            },
-            child: const Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.home),
-                Text("Home"),
-              ],
+        body: Column(
+          children: [
+            // w1
+            Container(
+              height: 70,
+              width: 200,
+              child: Image.network(
+                "https://images.unsplash.com/photo-1773332611476-6ec2ba68049f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-        ],
+
+            SizedBox(height: 30),
+
+            // w2
+            Text(
+              "Nepal's Most affordable IT training ",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            // Orange Container
+            Center(
+              child: Container(
+                height: 50,
+                width: 226,
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Center(
+                  child: Text(
+                    "Quality Education Without Financial Barriers",
+                  ),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 30),
+
+            // w3
+            FilledButton(
+              onPressed: () {},
+              child: Text("Click me"),
+            ),
+
+            SizedBox(height: 30),
+
+            // w4
+            Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.home),
+                  SizedBox(width: 30),
+                  Expanded(
+                    child: Text(
+                      "Get job-ready with live classes,\n"
+                      " lifetime access, and a guaranteed internship \n"
+                      "- all for less than Rs.2,500.",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
